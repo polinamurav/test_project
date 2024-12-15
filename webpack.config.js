dd7592db92522078ca4de4cce09f85a4edecc5e7
Polina Muravieva <polinamur3@gmail.com>
2024-12-15 23:25:43 +0300

    project basis

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                quietDeps: true,
+                            },
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+}
